1f4b8427aff7c34bc3f37a7e65d82547268d7255
Joeya1ds <90528531+Joeya1ds@users.noreply.github.com>
2022-04-23 13:16:24 -0400

    Update Ping.ps1

--- Ping.ps1
+++ Ping.ps1
@@ -1,7 +1,7 @@
 #Created by Joeya1ds 3/25/22
 
 $filepath = "" #Enter filepath here
-$hostname = read-host -Prompt 'Enter computer mneumonic or IP address'
+$hostname = read-host -Prompt 'Enter computer name or IP address'
 New-Item -Path $filepath -Name "$hostname.txt" -ItemType "file"
 Start-Transcript -path $filepath\$hostname.txt -Append
 Ping.exe -t $hostname | ForEach {"{0} - {1}" -f (Get-Date),$_}
